592a2248c3b495342b6b919adfce7198c1d9f3ce
IrfanFared <akunytberita527>
2024-06-10 14:39:13 +0700

    bealajar dart dasar

---
+++ lib/field.dart
@@ -0,0 +1,12 @@
+class Biodata {
+  String nama = "irfan";
+  String? alamat;
+  final negara = "indonesia";
+}
+
+void main() {
+  var biodatakecil = Biodata();
+  print(biodatakecil.alamat);
+}
+
+/// field /propetis/stribut adalah data yang bisa disisipkan di object
---
+++ lib/fist.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("halo dart oop");
+}
+
+
+///oop terdiri dari onject dan class
+///object berisi field/propreties/atribut / metotds/funtio/dan behavior
+///di date dart semua adalah object
+///class adalah cetekan untuk membuat object
+///clas bisa dibuata tidak terbatas
