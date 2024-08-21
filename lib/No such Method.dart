4eb6f7ac79a832a309fa01567842b059549537e9
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-08-21 22:15:27 +0700

    baru pertama kali eror bikin pusing

---
+++ lib/No such Method.dart
@@ -0,0 +1,18 @@
+import 'dart:nativewrappers/_internal/vm/lib/mirrors_patch.dart';
+
+class Repository {
+  final String _name;
+  Repository(this._name);
+  dynamic noSuchMethod(Invocation invocation) {
+    var column = MirrorSystem.getName(invocation.memberName);
+    var value = invocation.positionalArguments.first;
+
+    var sql = "select * from $_name where $column = $value'";
+    print(sql);
+  }
+}
+
+void main() {
+  dynamic repository = Repository("product");
+  repository.id("1234");
+}
