36331e8c7986ca43772bc8d0752ec29d0549236a
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-08-23 15:17:35 +0700

    kebantu banget karena ai

--- lib/No such Method.dart
+++ lib/No such Method.dart
@@ -1,22 +1,39 @@
-// import 'dart:nativewrappers/_internal/vm/lib/mirrors_patch.dart';
+class MyDynamicClass {
+  final Map<String, dynamic> _properties = {};
 
-// class Repository {
-//   final String _name;
-//   Repository(this._name);
-//   dynamic noSuchMethod(Invocation invocation) {
-//     var column = MirrorSystem.getName(invocation.memberName);
-//     var value = invocation.positionalArguments.first;
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    var memberName = invocation.memberName.toString();
+    memberName = memberName.substring(
+        8, memberName.length - 2); // Remove Symbol('') wrapper
 
-//     var sql = "select * from $_name where $column = $value'";
-//     print(sql);
-//   }
-// }
+    if (invocation.isMethod) {
+      print(
+          'Method $memberName dipanggil dengan argumen ${invocation.positionalArguments}');
+    } else if (invocation.isGetter) {
+      print('Getter untuk $memberName dipanggil');
+      return _properties[memberName];
+    } else if (invocation.isSetter) {
+      print(
+          'Setter untuk $memberName dipanggil dengan nilai ${invocation.positionalArguments.first}');
+      _properties[memberName] = invocation.positionalArguments.first;
+    }
 
-// void main() {
-//   dynamic repository = Repository("product");
-//   repository.id("123s4");
-// }
- 
+    return null;
+  }
+}
 
+void main() {
+  dynamic obj = MyDynamicClass();
 
- //error
+  // Memanggil metode yang tidak ada
+  obj.someMethod(42, 'example');
+
+  // Mengakses getter untuk properti yang tidak ada
+  var value = obj.someProperty;
+  print('Nilai someProperty: $value');
+
+  // Mengatur nilai pada properti yang tidak ada
+  obj.someProperty = 'newValue';
+  print('Nilai someProperty setelah diubah: ${obj.someProperty}');
+}
