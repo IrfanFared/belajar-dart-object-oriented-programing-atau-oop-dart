fe6ff737e6a174ca48d14e81f2643a5be04526aa
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-08-24 12:50:27 +0700

    belajar no such metod di dart

---
+++ lib/no_such_metod_absrak_class.dart
@@ -0,0 +1,23 @@
+abstract class AbstractClass {
+  // Metode ini akan dipanggil ketika metode yang tidak ada dipanggil
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    print("Metode ${invocation.memberName} tidak ditemukan");
+    return super.noSuchMethod(invocation);
+  }
+}
+
+class ConcreteClass extends AbstractClass {
+  void existingMethod() {
+    print("Ini adalah metode yang ada");
+  }
+}
+
+void main() {
+  var obj = ConcreteClass();
+  obj.existingMethod(); // Output: Ini adalah metode yang ada
+//   obj.nonExistentMethod(); // Output: Metode Symbol("nonExistentMethod") tidak ditemukan
+}
+
+ // jado do  no such metod di abstrak artinya no such metod yang di batasi jika ada metod yang di ada di field paren makan bakalan ero
+// 
