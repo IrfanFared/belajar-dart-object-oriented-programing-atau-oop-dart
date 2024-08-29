9453922f66010873ee1d9b86a95e161d980ef787
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-08-29 13:31:54 +0700

    siang koding

---
+++ lib/menangkap_objek_ execption.dart
@@ -0,0 +1,24 @@
+class ValidationException implements Exception {
+  String? masage;
+  ValidationException(this.masage);
+}
+
+class Validation {
+  static void validate(String username, String pasword) {
+    if (username == "") {
+      throw ValidationException("username kosong");
+    } else if (pasword == "") {
+      throw ValidationException("pasword kosong");
+    }
+  }
+}
+
+void main() {
+  try {
+    Validation.validate("w", "");
+  } on ValidationException catch (eror) {
+    print('exception :${eror.masage}');
+  }
+}
+
+// try-catch dalam bahasa pemrograman Dart adalah sebuah mekanisme yang digunakan untuk menangani kesalahan (error) atau pengecualian (exception) yang mungkin terjadi selama eksekusi program. Dengan menggunakan try-catch, Anda dapat mengendalikan bagaimana program bereaksi terhadap kesalahan, mencegah program agar tidak langsung berhenti saat terjadi error, dan memungkinkan program untuk menangani error dengan cara yang lebih baik.
