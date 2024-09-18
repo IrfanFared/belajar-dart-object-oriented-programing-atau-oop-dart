dc59c1e4504222440c8ad793f307def1dd424876
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-09-18 11:02:17 +0700

    membuat sutudy case lain tapi belum selesai

---
+++ lib/enum2.dart
@@ -0,0 +1,12 @@
+enum Arah { utara, selatan, barat, timur }
+
+class PenunjukArah {
+  var arahrumah = Arah.utara;
+
+  void cek() {}
+}
+
+void main() {
+  var penujukarah = PenunjukArah();
+  penujukarah.cek();
+}
